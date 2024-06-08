0444fd853349431d9d7e4603be7c30aae2178476
wliwh <wanghn0409@gmail.com>
2024-06-08 22:54:27 +0800

    更新

--- draw_chart/draw_qvix.py
+++ draw_chart/draw_qvix.py
@@ -1,12 +1,11 @@
 from index_get.get_funds_value import basic_index_getter
 import pandas as pd
 from common.chart_core import make_candle_echarts
+from pyecharts.charts import Tab
 
 Beg, End = '2019-01-04', '2024-05-31'
 
 # szzs = basic_index_getter('000001','2019-01-04','2024-05-31')
-sz50 = basic_index_getter('000016', Beg, End)
-# hs300 = basic_index_getter('000300','2019-01-04','2024-04-19')
 # zz1000 = basic_index_getter('000852', '2019-01-04','2024-04-19')
 
 def corr_mat_plt():
@@ -22,11 +21,24 @@ def corr_mat_plt():
 
 
 def draw_qvix_ratio(cword='50ETF'):
+    if cword=='50ETF':
+        ohlc = basic_index_getter('000016', Beg, End)
+    else:
+        ohlc = basic_index_getter('000300',Beg, End)
     p1 = pd.read_csv('../data_save/qvix_day.csv',index_col=0)
     p1 = p1.query("code=='{}'".format(cword))
-    sz50['qval'] = p1['close']
-    sz50['qrat'] = p1['cp_div']
-    tend = make_candle_echarts(sz50, Beg, End,'open high low close volume'.split(), plt_title_opts={'title':cword}, plt_add_ma=(20,60,240),other_tbs=[{'line':sz50['qval']},{'line':sz50['qrat']}])
-    tend.render('qworks.html')
+    ohlc['qval'] = p1['close']
+    ohlc['qrat'] = p1['cp_div']
+    tend = make_candle_echarts(ohlc, Beg, End,'open high low close volume'.split(), plt_title_opts={'title':cword}, plt_add_ma=(20,60,240),other_tbs=[{'line':ohlc['qval']},{'line':ohlc['qrat']}])
+    return tend
 
-draw_qvix_ratio()
+def draw_qvix_echarts():
+    clst = ('50ETF', '300ETF')
+    tab = Tab()
+    for c in clst:
+        tend = draw_qvix_ratio(c)
+        tab.add(tend,c)
+    tab.render('rbs.html')
+
+
+draw_qvix_echarts()
